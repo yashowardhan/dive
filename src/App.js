630663b317d7013f0877336e2832b9db4551cc48
Yash <yashkaushikbits19@gmail.com>
2020-06-24 01:33:40 +0530

    Updated with Material UI
    Updated with Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from "react";
 import Home from "./components/home/index";
-import RegisterForm from "./components/home/register";
-import Topic from "./components/topic/index";
-import SuggestionPage from "./components/suggestion/index";
+import SignUp from "./components/signup/SignUp";
+import MediaCard from "./components/card/Card";
+import Recommend from "./components/recommend/Recommend";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/register" component={RegisterForm} />
-          <Route path="/topics" component={Topic} />
-          <Route path="/recommend" component={SuggestionPage} />
+          <Route exact path="/" component={MediaCard} />
+          <Route exact path="/signin" component={Home} />
+          <Route path="/signup" component={SignUp} />
+          {/* <Route path="/topics" component={Topic} /> */}
+          <Route path="/recommend" component={Recommend} />
           {/* <Route path="/course/:slug" component={ManageCoursePage} />
           
           <Route component={PageNotFound} />   */}
         </Switch>
       </BrowserRouter>
+      {/* <Footer /> */}
     </div>
   );
 }
